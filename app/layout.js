e0473649e9f09dfb90f8937a4fdb41ece634c031
Lakshya Kashyap <yashjha0241@gmail.com>
2024-06-23 17:00:38 +0530

    New Ui/Ux

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
+import { jost } from "./font";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Jost } from "next/font/google";
 
 export const metadata = {
   title: "Simple Todo",
@@ -11,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jost.className}>{children}</body>
     </html>
   );
 }
