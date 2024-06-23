bd091319fc9703ae0f0e3ef65e1d7f357e312610
Lakshya Kashyap <yashjha0241@gmail.com>
2024-06-23 17:22:34 +0530

    Improved Ui/Ux

--- app/font.js
+++
@@ -1,6 +0,0 @@
-import { Jost } from 'next/font/google'
- 
-export const jost = Jost({
-  subsets: ['latin'],
-  display: 'swap',
-})
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
-import { jost } from "./font";
 import "./globals.css";
-import { Jost } from "next/font/google";
+import { Jost } from 'next/font/google'
+
+const jost_init = Jost({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-jost',
+});
 
 export const metadata = {
   title: "Simple Todo",
@@ -10,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jost.className}>{children}</body>
+      <body className={jost_init.variable}>{children}</body>
     </html>
   );
 }
